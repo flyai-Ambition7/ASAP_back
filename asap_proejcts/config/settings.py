ffe9c49649f745595f164705a164751196cab410
Choijian <an032944@daum.net>
2024-02-12 02:10:34 +0900

    Feat : users.py 추가

--- asap_proejcts/config/settings.py
+++ asap_proejcts/config/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "djongo"
+    "djongo",
+    # 앱
+    "asap",
+    # 라이브러리
+    "rest_framework",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
