b76fd58d5879906a2c213e5aedebb84612dc2fb6
Choijian <an032944@daum.net>
2024-02-13 17:01:42 +0900

    Refactor : username을 이용한 로그인

--- asap_proejcts/users/views.py
+++ asap_proejcts/users/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserSerializer
 from .models import User
+from django.utils import timezone
 
 # Create your views here.
 class RegisterView(APIView):
@@ -11,4 +13,33 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+        user = User.objects.filter(username=username).first()
+                
+        # user가 없는 경우
+        if user is None:
+            raise AuthenticationFailed('User Not Found!')
+        
+        # 비밀번호가 일치하지 않는 경우
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+
+        # 토큰 생성
+        refresh = RefreshToken.for_user(user)
+
+        # 토큰 payload
+        payload = {
+            'id': user.id,
+            'exp': timezone.now() + timezone.timedelta(minutes=60),
+            'iat': timezone.now()
+        }
+
+        # 토큰 반환
+        return Response({
+            'access_token': str(refresh.access_token),
+            'refresh_token': str(refresh)
+        })
