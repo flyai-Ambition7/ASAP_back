79482fa58cdcf07d098fcde2929e46bd715f2a86
Choijian <an032944@daum.net>
2024-02-13 17:04:42 +0900

    Refactor : 사용자 관련 view 명칭 통일

--- asap_proejcts/users/urls.py
+++ asap_proejcts/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RegisterView
+from .views import SignupView, LoginView, LogoutView()
 
 urlpatterns = [
-    path('register', RegisterView.as_view()),
+    path('signup', SignupView.as_view()),
+    path('login', LoginView.as_view()),
+    path('logout', LogoutView.as_view())
 ]
