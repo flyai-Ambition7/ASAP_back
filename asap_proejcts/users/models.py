9478eefa061813cc7ed4e69c82c9d5aa2d185713
Choijian <an032944@daum.net>
2024-02-13 16:49:11 +0900

    Fix : User 정보에 userid 추가

--- asap_proejcts/users/models.py
+++ asap_proejcts/users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    userid = models.CharField(unique=True, max_length=50) # 사용자 아이디
     name = models.CharField(max_length=20) # 사용자 이름
     email = models.EmailField(unique=True) # 사용자 이메일
     phone = models.CharField(max_length=12) # 사용자 전화번호
