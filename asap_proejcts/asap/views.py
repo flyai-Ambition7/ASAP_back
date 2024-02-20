7d0374857629f75a6878f9d8750a1bf12a72c587
dbp-jack <dbp100402@gmail.com>
2024-02-20 15:19:23 +0900

    Feat: ai모델 연동, ai모델 데이터 추가

--- asap_proejcts/asap/views.py
+++ asap_proejcts/asap/views.py
@@ -13,7 +13,7 @@ from config.settings import OPENAI_API_KEY
 #     queryset = CommonInfo.objects.all()
 #     serializer_class = CommonInfoSerializer
 
-class ItemInfoViewSet(viewsets.ModelViewSet):
+class ItemInfoViewSet(viewsets.ModelViewSet): # GPT지안이 넣은 상태, 여기서 추가로 폰트, 배경을 넣으면 된다.
     queryset = ItemInfo.objects.all()
     serializer_class = ItemInfoSerializer
     
@@ -64,3 +64,68 @@ class ItemInfoViewSet(viewsets.ModelViewSet):
 class ResultDataViewSet(viewsets.ModelViewSet):
     queryset = ResultData.objects.all()
     serializer_class = ResultDataSerializer
+
+#############-----------\
+import time
+from draw_image import draw_filtered_image_by_DALLE, draw_image_by_SD
+from edit_image import add_images
+# from eval_image import evalulate_image
+#생성시간 나중에 테이블 추가
+        # 데이터베이스에서 필요한 정보들을 읽어오는 함수 호출 -> 디비 데이터변수명 수정
+        # 입력받은 이미지, 텍스트 프롬프트(폰트), 이미지 프롬프트(배경)
+        # image, summarized_copy, text_prompt, background_prompt, result_image_url
+class ResultDataViewSet(viewsets.ModelViewSet):
+    queryset = ResultData.objects.all()
+    serializer_class = ResultDataSerializer
+
+    def draw(self, request, *args, **kwargs):
+        start = time.time()
+        result_data_instance = ResultData.objects.first()
+        # 가져온 데이터를 변수에 할당
+        image = result_data_instance.image
+        summarized_copy = result_data_instance.summarized_copy
+        text_prompt = result_data_instance.text_prompt
+        background_prompt = result_data_instance.background_prompt
+
+        # DALLE 모델을 이용하여 이미지를 그리는 함수 호출
+        DALLE_img, DALLE_acc = draw_filtered_image_by_DALLE(text_prompt)
+        # SD 모델을 이용하여 이미지를 그리는 함수 호출
+        SD_img = draw_image_by_SD(image, background_prompt)
+        # 두 이미지를 결합하는 함수 호출
+        img_output = add_images(DALLE_img, SD_img)
+
+        serializer = ResultDataSerializer(data={
+            'result_image_url': img_output
+        })
+
+        if serializer.is_valid():
+            serializer.save()
+            end = time.time()
+            return Response({
+                "DALLE_accuracy": f"{DALLE_acc:.2f}",
+                "time_consumption": f"{end - start:.2f}"
+            })
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# def draw():
+#     start = time.time()
+#     # 데이터베이스에서 필요한 정보들을 읽어오는 함수 호출
+#     img_input, img_file_name, text_prompt, image_prompt = read_infos_from_db(img_chunk_tbl, img_meta_tbl, text_tbl, False)
+#     user_id = img_file_name.split('_')[0]
+#     # DALLE 모델을 이용하여 이미지를 그리는 함수 호출
+#     DALLE_img, DALLE_acc = draw_filtered_image_by_DALLE(text_prompt)
+#     # SD 모델을 이용하여 이미지를 그리는 함수 호출
+#     SD_img = draw_image_by_SD(img_input, image_prompt)
+#     # 두 이미지를 결합하는 함수 호출
+#     img_output = add_images(DALLE_img, SD_img)
+#     # 결과 이미지를 데이터베이스에 업데이트하는 함수 호출
+#     update_image_to_db(img_output, user_id, upload_time=int(start), isinput=False)
+#     end = time.time()
+#     return {
+#         "DALLE_accuracy": f"{DALLE_acc:.2f}",
+#         "time_consumption": f"{end - start:.2f}"
+#     }
+#         image = request.data.get('image')
+#         summarized_copy = request.data.get('summarized_copy')
+#         text_prompt = request.data.get('text_prompt')
+#         background_prompt = request.data.get('background_prompt')
