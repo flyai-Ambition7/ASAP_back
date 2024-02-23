ff1783183dc62a39a524af88e495cfadd18fd2a7
choijian <an032944@daum.net>
2024-02-23 10:09:54 +0900

    Fix : SD 프롬프트 수정

--- asap_proejcts/asap/views.py
+++ asap_proejcts/asap/views.py
@@ -51,7 +51,7 @@ class TextImageGenerator():
 
     # OpenAI DALL-E를 사용하여 이미지 생성하는 함수
     def draw_image_by_dalle(self):
-        prompt = f"Text '{self.text}' on a white background, minimalism" # text 이미지를 생성하기 위한 프롬프트
+        prompt = f"one line Text '{self.text}' on a white background, minimalism" # text 이미지를 생성하기 위한 프롬프트
 
         client = openai(api_key=OPENAI_API_KEY)
         # DALL-E 모델을 사용하여 주어진 프롬프트로 이미지를 생성합니다. 이미지 크기는 1792x1024, 품질은 hd, 생성할 이미지 수는 1입니다.
@@ -164,9 +164,9 @@ class BgImageGenerator(): #img, product_name, description,theme, result_type
         # pipe.to("cuda")  # CUDA를 사용하여 GPU에서 모델 실행
 
         # 긍정적 및 부정적 프롬프트 설정
-        prompt = f"create an advertising image for {self.result_type} suitable for promoting {self.result_type}."
-        pos = f"{self.theme}"
-        neg = f"text, worst, bad, (distorted:1.3), (deformed:1.3), (blurry:1.3), out of frame, duplicate, (text:1.3), (render:1.3)"
+        prompt = f"create an advertising background to promote {self.product_name} in the {self.result_type} advertising style."
+        pos = f"advertisement, {self.theme}, Bokeh"
+        neg = f"(person : 1.5), (woman :1.5), face, worst, bad, (distorted:1.3), (deformed:1.3), (blurry:1.3), out of frame, duplicate, (text:1.3), (render:1.3)"
 
         # 각 이미지에 대해 다른 무작위 시드를 사용하여 결과 다양성 보장
         #generator = [torch.Generator(device="cuda").manual_seed(i) for i in range(2)]
